9a93f4c93c3ba88750d9b51df6afac75faeb33fc
Alihan Zihna <alihanz@gmail.com>
2020-07-23 12:49:48 +0100

    Removed sudo commands from each line

--- .set_init2.sh
+++ .set_init2.sh
@@ -8,31 +8,30 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $(echo $HOME)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $(echo $HOME)/.oh-my-zsh/custom/themes/powerlevel10k
 
 # install alacritty
-sudo add-apt-repository ppa:mmstick76/alacritty
-sudo apt update
-sudo apt install alacritty
+add-apt-repository ppa:mmstick76/alacritty
+apt update
+apt install alacritty
 
 # install python
-sudo apt-get install python3 
-sudo apt-get install python3-pip
+apt-get install python3 
+apt-get install python3-pip
 # for Neovim deoplete
 pip3 install --upgrade msgpack==1.0.0
 pip3 install --user jedi
 
 # Install neovim
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim
+add-apt-repository ppa:neovim-ppa/stable
+apt-get update
+apt-get install neovim
 
 # Install python3 neovim interface
-sudo apt-get install python3-neovim
+apt-get install python3-neovim
 
 # Make sure a clipboard tool is installed
-sudo apt-get install xclip
+apt-get install xclip
 
 # Install tmux
-sudo apt-get install tmux
+apt-get install tmux
 
 # Install Stow
-sudo apt-get install stow
-
+apt-get install stow
